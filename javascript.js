4d9736f58fd4ca16e4666eb270717e51b644e20f
Sayed-Nabil-Habib <nabilhabib2015@yahoo.com>
2023-05-04 17:29:31 +0200

    slide photos added

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 const apiBase = "https://www.sayed.codes";
 const pagePosts = "/wp-json/wp/v2/posts/";
-const fullPageUrl = apiBase + pagePosts;
+const fullPageUrl = apiBase + pagePosts + "?per_page=20";
 
 const resultContainer = document.querySelector(".slider")
 
@@ -15,13 +15,16 @@ async function getPosts(){
 
   for(let i = 0; i < data.length; i++){
 
+    const mediaResponse = await fetch(data[i]._links["wp:featuredmedia"][0].href);
+    const mediaData = await mediaResponse.json();
+
     resultContainer.innerHTML += 
     `
   <div class="destinations">
     <div class="slider">
       <div class="slide">
         <a href="#">
-          <img class="posts-images" src="${data[i].featured_media}" alt="${data[i].title.rendered}">
+          <img class="posts-images" src="${mediaData.source_url}" alt="${data[i].title.rendered}">
             <h5 class="title">  ${data[i].title.rendered}</h5>
             <p class="content">  ${data[i].excerpt.rendered}</p>
             <h4 class="read-more">Read more</h4>

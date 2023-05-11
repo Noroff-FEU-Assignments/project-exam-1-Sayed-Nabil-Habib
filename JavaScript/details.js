1f4a3636d74fb9c6cfbe19dae2c80f34bd9cc371
Sayed-Nabil-Habib <nabilhabib2015@yahoo.com>
2023-05-11 10:50:00 +0200

    overlay of images added

--- JavaScript/details.js
+++ JavaScript/details.js
@@ -23,15 +23,34 @@ async function fetchDetails(){
     const title = data.title.rendered;
     const content = data.content.rendered;
     const imageUrl = data._links["wp:featuredmedia"][0].href;
+    const mediaResponse = await fetch(imageUrl);
+    const mediaData = await mediaResponse.json();
+    const featuredImageUrl = mediaData.source_url;
   
       resultContainer.innerHTML += 
       `<div class="result">
       <h5 class="title">  ${title}</h5>   
-      <img src="${imageUrl}" alt="${title}" class="post-image">
+      <img src="${featuredImageUrl}" alt="${title}" class="post-image clickable">
         <div class="content">${content}</div> 
         <a href="../index.html" class="btn">To Home Page</a> 
      </div>`;
-  
+
+     const clickableImages = resultContainer.querySelector(".clickable");
+     clickableImages.addEventListener("click", () =>{
+      const overlay = document.createElement("div");
+      overlay.classList.add("overlay");
+
+      const largedImage = document.createElement("img");
+      largedImage.src = featuredImageUrl;
+      largedImage.alt = title;
+
+      overlay.appendChild(largedImage);
+      document.body.appendChild(overlay);
+
+      overlay.addEventListener("click", () => {
+        document.body.removeChild(overlay)
+      })
+     })
        
      
     }catch(error){

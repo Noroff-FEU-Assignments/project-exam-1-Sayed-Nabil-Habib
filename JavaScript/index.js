f07082a355315f1722f7bbb0deef9a67e2259c4e
Sayed-Nabil-Habib <nabilhabib2015@yahoo.com>
2023-05-16 11:14:55 +0200

    js updated a
    required js code has been written

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,41 +1,56 @@
 const apiBase = "https://www.sayed.codes";
 const pagePosts = "/wp-json/wp/v2/posts/";
-const fullPageUrl = apiBase + pagePosts + "?per_page=20";
 
-const resultContainer = document.querySelector(".slider")
+let currentPage = 1;
+const perPage = 10;
+let allPostLoaded = false;
 
+const resultContainer = document.querySelector(".slider");
+const showMoreBtn = document.querySelector("#show_more");
 
+async function getPosts() {
+  try {
+    const fullPageUrl = `${apiBase}${pagePosts}?per_page=${perPage}&page=${currentPage}`;
+    const response = await fetch(fullPageUrl);
+    const data = await response.json();
 
-async function getPosts(){
+    // resultContainer.innerHTML = "";
 
-  const response = await fetch(fullPageUrl);
-  const data = await response.json();
+    for (let i = 0; i < data.length; i++) {
+      const mediaResponse = await fetch(data[i]._links["wp:featuredmedia"][0].href);
+      const mediaData = await mediaResponse.json();
 
-  resultContainer.innerHTML = "";
-
-  for(let i = 0; i < data.length; i++){
-
-    const mediaResponse = await fetch(data[i]._links["wp:featuredmedia"][0].href);
-    const mediaData = await mediaResponse.json();
-
-    resultContainer.innerHTML += 
-    `
-  <div class="destinations">
-    <div class="slider">
-      <div class="slide">
-      <a href="../htmls/details.html?id=${data[i].id}">
-
-          <img class="posts-images" src="${mediaData.source_url}" alt="${data[i].title.rendered}">
-            <h5 class="title">  ${data[i].title.rendered}</h5>
-            <p class="content">  ${data[i].excerpt.rendered}</p>
-            <h4 class="read-more">Read more</h4>
-            </a>
+      resultContainer.innerHTML += `
+        <div class="destinations">
+          <div class="slider">
+            <div class="slide">
+            <a href="../htmls/details.html?id=${data[i].id}">
+                <img class="posts-images" src="${mediaData.source_url}" alt="${data[i].title.rendered}">
+                <h5 class="title">${data[i].title.rendered}</h5>
+                <p class="content">${data[i].excerpt.rendered}</p>
+                <h4 class="read-more">Read more</h4>
+              </a>
             </div>
-               </div>
-                
-</div>`
+          </div>
+        </div>`;
+    }
+
+    if (data.length < perPage) {
+      allPostLoaded = true;
+      showMoreBtn.style.display = "none";
+    }
+  } catch (error) {
+    resultContainer.innerHTML += `<div>Could not fetch data, please try again later</div>`;
+    console.log("Error:", error);
   }
-  
-   console.log(data)
+}
+async function loadMorePosts() {
+  if (!allPostLoaded) {
+    currentPage++;
+    await getPosts();
+  }
 }
 getPosts();
+
+
+showMoreBtn.addEventListener("click", loadMorePosts);

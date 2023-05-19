7e9af17f3f4463969e9be86eac053c6af644f12a
Sayed-Nabil-Habib <nabilhabib2015@yahoo.com>
2023-05-19 12:19:16 +0200

    contact form.validation

--- JavaScript/contact.js
+++ JavaScript/contact.js
@@ -1,8 +1,8 @@
 const form = document.querySelector("#contact-form");
-const name = document.querySelector("#name");
-const email = document.querySelector("#email");
-const subject = document.querySelector("#subject");
-const message = document.querySelector("#message");
+const nameInput = document.querySelector("#name");
+const emailInput = document.querySelector("#email");
+const subjectInput = document.querySelector("#subject");
+const messageInput = document.querySelector("#message");
 
 const nameError = document.querySelector("#nameError");
 const emailError = document.querySelector("#emailError");
@@ -12,49 +12,54 @@ const messageError = document.querySelector("#messageError");
 const successMessage = document.querySelector("#contact-success-message");
 const contactTitle = document.querySelector("#contact-title");
 
-function validateForm(event){
+function validateForm(event) {
   event.preventDefault();
 
-  if(checkLength(name.value, 0)=== true){
+  const nameValue = nameInput.value.trim();
+  const emailValue = emailInput.value.trim();
+  const subjectValue = subjectInput.value.trim();
+  const messageValue = messageInput.value.trim();
+
+  if (nameValue.length >= 5) {
     nameError.style.display = "none";
-  }else{
-    nameError.style.display = "block"
+  } else {
+    nameError.style.display = "block";
   }
-  if(validateEmail(email.value, 0)=== true){
-    emailError.style.display ="none";
-  }else{
+
+  if (validateEmail(emailValue)) {
+    emailError.style.display = "none";
+  } else {
     emailError.style.display = "block";
   }
-  if(checkLength(subject.value, 0)=== true){
+
+  if (subjectValue.length >= 15) {
     subjectError.style.display = "none";
-    }else{
-      subjectError.style.display = "block";
-    }
-    if(checkLength(message.value, 0) === true){
-      messageError.style.display = "none";
-    }else{
-      messageError.style.display = "block";
-    }
+  } else {
+    subjectError.style.display = "block";
+  }
 
-    if(name.value && email.value && validateEmail(email.value) && subject.value && message.value){
-      successMessage.style.display ="block";
-      contactTitle.style.display = "none";
-      form.reset();
-     
-    }
+  if (messageValue.length >= 24) {
+    messageError.style.display = "none";
+  } else {
+    messageError.style.display = "block";
+  }
+
+  if (
+    nameValue.length >= 4 &&
+    validateEmail(emailValue) &&
+    subjectValue.length >= 15 &&
+    messageValue.length >= 24
+  ) {
+    
+    successMessage.style.display = "block";
+    contactTitle.style.display = "none";
+    form.reset();
+  }
 }
-  form.addEventListener("submit", validateForm);
 
-  function checkLength(value, len){
-    if(value.trim().length > len){
-      return true;
-    }else{
-      return false;
-    }
-  }
-  function validateEmail(email){
-    const regEx = /^\S+@\S+\.\S+$/;
-    const patterMatches = regEx.test(email);
-    return patterMatches;
-  }
-  
+form.addEventListener("submit", validateForm);
+
+function validateEmail(email) {
+  const regEx = /^\S+@\S+\.\S+$/;
+  return regEx.test(email);
+}
